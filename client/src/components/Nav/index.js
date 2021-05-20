4b410258ca0ca2f642cc04e3b311eb5be78fe461
Eric Hay <ericdanielhay@yahoo.com>
2021-05-19 22:03:17 -0500

    Updated styling on Navbar and removed two jumbotrons.

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <a className="navbar-brand" href="/">
+        (React) Google Books Search
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
