550f0de2b55d1e2f7e872941b4e176bf2e28f24d
Eric Hay <ericdanielhay@yahoo.com>
2021-12-03 16:13:45 -0600

    Added nav back into login form.

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -3,43 +3,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function LoginForm(props) {
   return (
-    <form>
-      <div className="form-group">
-        <label for="email">Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          name="email"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-          onChange={props.handleEmailChange}
-        />
-      </div>
-      <div className="form-group">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          className="form-control"
-          name="password"
-          placeholder="Password"
-          onChange={props.handlePasswordChange}
-        />
-      </div>
-      <button
-        type="submit"
-        className="btn btn-primary"
-        onClick={props.handleInSubmit}
-      >
-        Log In
-      </button>
-      <button
-        type="submit"
-        className="btn btn-secondary"
-        onClick={props.handleUpSubmit}
-      >
-        Sign Up
-      </button>
-    </form>
+    <nav className="navbar navbar-light">
+      <form>
+        <div className="form-group">
+          <label for="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            onChange={props.handleEmailChange}
+          />
+        </div>
+        <div className="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="Password"
+            onChange={props.handlePasswordChange}
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={props.handleInSubmit}
+        >
+          Log In
+        </button>
+        <button
+          type="submit"
+          className="btn btn-secondary"
+          onClick={props.handleUpSubmit}
+        >
+          Sign Up
+        </button>
+      </form>
+    </nav>
   );
 }
 
